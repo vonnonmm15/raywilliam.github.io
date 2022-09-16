94a1a9af53fcc9d642a25f7a3ca697ed461b406a
vonnonmm15 <110140443+vonnonmm15@users.noreply.github.com>
2022-09-15 22:29:49 -0500

    gi

---
+++ Assignments/js practice/js/hello.js
@@ -0,0 +1,47 @@
+function convertToDaysLeft(milliseconds) {
+    return Math.floor(milliseconds / (1000 * 60 * 60 * 24));
+  }
+
+  function convertToHoursLeft(milliseconds) {
+    return Math.floor(
+      (milliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+  }
+
+  function convertToMinutesLeft(milliseconds) {
+    return Math.floor(
+      (milliseconds % (1000 * 60 * 60)) / (1000 * 60)
+    );
+  }
+
+  function convertToSecondsLeft(milliseconds) {
+    return Math.floor(
+      (milliseconds % (1000 * 60)) / 1000
+    );
+  }
+
+  function formatCountdownText(days, hours, minutes, seconds) {
+    return `${days}d ${hours}h ${minutes}m ${seconds}s left`;
+  }
+
+  let deadline = new Date ("October 31, 2022 13:00:00");
+
+  let countdown =  document.getElementById("countdown");
+
+  countdown.innerHTML = " ";
+
+  function setCountdown () {
+            let now = new Date();
+            let timeLeft = deadline.getTime() - now.getTime();
+
+
+
+  let daysLeft = convertToDaysLeft(timeLeft);
+  let hoursLeft = convertToDaysLeft(timeLeft);
+  let minutesLeft = convertToDaysLeft(timeLeft);
+  let secondsLeft = convertToDaysLeft(timeLeft);
+
+  countdown.innerHTML = formatCountdownText(daysLeft, hoursLeft, minutesLeft , secondsLeft);
+  }
+
+  setInterval(setCountdown, 1000)
